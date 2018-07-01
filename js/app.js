bb83f3b4bf7e47c7bb58fdcd8b2718b34e8c4301
khorramk <khorramk.kbsk@gmail.com>
2018-07-01 19:31:18 +0100

    making the neccessary objects

--- js/app.js
+++ js/app.js
@@ -1,30 +1,48 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var allEnemies = [];
+class Enemy {
+    constructor(_x = 100, _y = 83, _speed) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.speed = _speed;
+        this._y += _y;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        this.width = 50;
+        this.height = 60
+        //iterate to increase the speed of movement
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        allEnemies.push(this)
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+    }
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Player {
+    constructor(_x, _y, _speed) {
+        this._x += _x;
+        this._y += _y;
+        
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
